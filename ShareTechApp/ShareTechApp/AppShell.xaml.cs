cb05d5305cc396e55491a0883fd5a79da8381a73
nsun527 <nsun527@naver.com>
2021-09-11 16:58:09 +0900

    Add project files.

---
+++ ShareTechApp/ShareTechApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ShareTechApp.ViewModels;
+using ShareTechApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ShareTechApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ ShareTechApp/ShareTechApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ShareTechApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ShareTechApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
